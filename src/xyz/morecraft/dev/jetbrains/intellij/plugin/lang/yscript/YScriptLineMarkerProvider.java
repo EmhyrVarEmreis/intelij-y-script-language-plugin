0c2a8863b7ce2caf14ee23e1b286041c6efb4ed9
Mateusz Stefaniak <mateusz.stefaniak@morecraft.pl>
2018-10-22 11:42:03 +0200

    Add - Line markers for program calls #2

--- src/xyz/morecraft/dev/jetbrains/intellij/plugin/lang/yscript/YScriptLineMarkerProvider.java
+++ src/xyz/morecraft/dev/jetbrains/intellij/plugin/lang/yscript/YScriptLineMarkerProvider.java
@@ -7,13 +7,41 @@ import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import org.jetbrains.annotations.NotNull;
 import xyz.morecraft.dev.jetbrains.intellij.plugin.lang.yscript.psi.YScriptCall;
+import xyz.morecraft.dev.jetbrains.intellij.plugin.lang.yscript.psi.YScriptPackage;
 import xyz.morecraft.dev.jetbrains.intellij.plugin.lang.yscript.psi.YScriptProgram;
 import xyz.morecraft.dev.jetbrains.intellij.plugin.lang.yscript.util.YScriptUtil;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class YScriptLineMarkerProvider extends RelatedItemLineMarkerProvider {
+//
+//    @Override
+//    public RelatedItemLineMarkerInfo getLineMarkerInfo(@NotNull PsiElement element) {
+//        if (element instanceof YScriptPackage && element.getParent() instanceof YScriptCall) return new RelatedItemLineMarkerInfo(
+//                element,
+//                element.getTextRange(),
+//                YScriptIcons.FILE,
+//                1,
+//                null,
+//                null,
+//                GutterIconRenderer.Alignment.CENTER
+//        );
+//        return null;
+//        if (element instanceof YScriptCall) {
+//            final YScriptCall yScriptCall = (YScriptCall) element;
+//            final YScriptPackage yScriptPackage = yScriptCall.getPackage();
+//            return new RelatedItemLineMarkerInfo(
+//                    yScriptPackage,
+//                    yScriptPackage.getTextRange(),
+//                    YScriptIcons.FILE,
+//                    1,
+//
+//                    );
+//        }
+//        return super.getLineMarkerInfo(element);
+//    }
 
     @Override
     protected void collectNavigationMarkers(@NotNull PsiElement element, @NotNull Collection<? super RelatedItemLineMarkerInfo> result) {
@@ -24,10 +52,14 @@ public class YScriptLineMarkerProvider extends RelatedItemLineMarkerProvider {
             final Project project = element.getProject();
             final List<YScriptProgram> properties = YScriptUtil.findPrograms(project, programName);
             if (properties.size() > 0) {
+                final List<YScriptPackage> targets = new ArrayList<>(properties.size());
+                for (YScriptProgram property : properties) {
+                    targets.add(property.getPackage());
+                }
                 NavigationGutterIconBuilder<PsiElement> builder =
                         NavigationGutterIconBuilder.create(YScriptIcons.FILE)
-                                .setTargets(properties)
-                                .setTooltipText("Navigate to program definition");
+                                .setTargets(targets)
+                                .setTooltipText("Navigate to " + programName + "()");
                 result.add(builder.createLineMarkerInfo(element));
             }
         }
